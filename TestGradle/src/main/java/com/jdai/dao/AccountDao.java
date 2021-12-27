ff69577109719dd66010d6f043dbba7993b70ab2
jennydai2011@gmail.com <jennydai2011@gmail.com>
2021-12-27 13:32:59 -0700

    learn gradle

---
+++ TestGradle/src/main/java/com/jdai/dao/AccountDao.java
@@ -0,0 +1,7 @@
+package com.jdai.dao;
+
+import java.util.List;
+
+public interface AccountDao {
+    public List findAll();
+}
---
+++ TestGradle/src/main/java/com/jdai/dao/AccountDaoImpl.java
@@ -0,0 +1,11 @@
+package com.jdai.dao;
+
+import java.util.List;
+
+public class AccountDaoImpl implements AccountDao{
+    @Override
+    public List findAll() {
+        System.out.println("find all return ok");
+        return null;
+    }
+}
