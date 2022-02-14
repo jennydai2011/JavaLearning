8b67dec15b254006699520738ca50f3809891dad
jennydai2011@gmail.com <jennydai2011@gmail.com>
2022-02-13 21:02:59 -0700

    PlaySlip

---
+++ PlaySlip/src/test/java/com/jdai/playslip/PlaySlipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdai.playslip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaySlipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
